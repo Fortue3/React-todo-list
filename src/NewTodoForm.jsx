b20a96f22937dbe72c57b98ad22d06cf159716f4
Fortue3 <kwe22002@byui.edu>
2024-11-09 00:56:47 +0200

    aaa

--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react"
 
-export function NewTodoForm({ onSubmit }) {
+export function NewTodoForm() {
   const [newItem, setNewItem] = useState("")
 
   function handleSubmit(e) {
     e.preventDefault()
-    if (newItem === "") return
-
-    onSubmit(newItem)
+    if(newItem ==="")return
+  
+    props.onSubmit(newItem)
 
     setNewItem("")
   }
