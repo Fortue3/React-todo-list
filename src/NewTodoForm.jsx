70d4d774a3a95308afdd1182dc379eef3e72a072
Fortue3 <kwe22002@byui.edu>
2024-11-09 22:10:21 +0200

    list

--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -1,29 +1,28 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export function NewTodoForm() {
-  const [newItem, setNewItem] = useState("")
+export function NewTodoForm({ onSubmit }) {
+  const [newItem, setNewItem] = useState("");
 
   function handleSubmit(e) {
-    e.preventDefault()
-    if(newItem ==="")return
-  
-    props.onSubmit(newItem)
+    e.preventDefault();
+    if (newItem === "") return;
 
-    setNewItem("")
+    onSubmit(newItem);
+    setNewItem("");
   }
 
   return (
     <form onSubmit={handleSubmit} className="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New Item</label>
-        <input
+        <input 
           value={newItem}
-          onChange={e => setNewItem(e.target.value)}
-          type="text"
-          id="item"
+          onChange={e => setNewItem(e.target.value)} 
+          type="text" 
+          id="item" 
         />
       </div>
       <button className="btn">Add</button>
     </form>
-  )
+  );
 }
